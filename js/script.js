3aa932337ceba25be5784a18df47e9928da3a6b9
J4rCen <=>
2023-04-22 16:47:52 +0300

    Добавлена кроссплатформенность

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let buttonMenu = document.querySelector("#open-menu");
+let navMenu = document.querySelector("#nav-menu");
+
+buttonMenu.addEventListener("click", () => navMenu.classList.toggle("closed"))
